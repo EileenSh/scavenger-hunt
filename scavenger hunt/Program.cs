9a4b919b1da2aae474eece262b106041ca928d25
Eileen Shu <yshu@smail.pembinatrails.ca>
2023-01-09 09:32:19 -0600

    Add project files.

---
+++ scavenger hunt/Program.cs
@@ -0,0 +1,17 @@
+﻿string input;
+int numberGuess = 0;
+
+do
+{
+    Console.WriteLine("What is a reserve?");
+    if(numberGuess>5)
+    {
+        Console.WriteLine("hint");
+    }
+    input= Console.ReadLine();
+    numberGuess++;
+}
+while(input != "answer");
+
+Console.WriteLine("answer is correct");
+numberGuess = 0;
